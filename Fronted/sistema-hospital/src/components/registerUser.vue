d7993fc01627b1a11dd114b8bff60dfda2b3c583
Daniela-AG15 <danaguilar1512@gmail.com>
2024-06-07 13:01:33 -0600

    modificacion de los formulario

--- Fronted/sistema-hospital/src/components/registerUser.vue
+++ Fronted/sistema-hospital/src/components/registerUser.vue
@@ -1,31 +1,34 @@
 <template>
 	<form>
-	<div class="py-16">
-	  <div class="flex bg-white rounded-lg shadow-lg overflow-hidden mx-auto max-w-sm lg:max-w-4xl">
-		<!-- Col -->
-		<div class="w-full h-auto bg-gray-400 dark:bg-gray-800 hidden lg:block lg:w-5/12 bg-cover rounded-l-lg"
-			 style="background-image: url(https://i.pinimg.com/550x/f9/13/05/f9130503f61225d775facb9047d7ca92.jpg)"></div>
-			<PersonaView></PersonaView>
-		  
-	
+	  <div class="py-16">
+		<div class="flex bg-white rounded-lg shadow-lg overflow-hidden mx-auto max-w-sm lg:max-w-4xl">
+		  <!-- Col -->
+		  <div class="w-full lg:w-5/12 bg-cover rounded-l-lg" style="background-image: url(https://i.pinimg.com/550x/f9/13/05/f9130503f61225d775facb9047d7ca92.jpg)">
+			<!-- You can adjust the width of the image column here -->
+		  </div>
+		  <div class="p-8">
+			<!-- Form content -->
+			<div class="mb-4">
+			  <PersonaView></PersonaView>
+			</div>
+			<div class="mb-4">
+			  <UsuarioView></UsuarioView>
+			</div>
+		  </div>
+		</div>
 	  </div>
-	</div>
-</form>
+	</form>
   </template>
   
-  <!-- <script>
-  export default {
-	name: 'RegisterForm',
-  };
-  </script> -->
   <script>
   import PersonaView from './personas.vue';
   import UsuarioView from './usuarios.vue';
-  export default{
-	  components:{
-		  PersonaView,
-		  UsuarioView
-	  }
+  
+  export default {
+	components: {
+	  PersonaView,
+	  UsuarioView
+	}
   }
   </script>
   
@@ -40,6 +43,8 @@
 	  background-color: #1a202c;
 	}
   }
+  
+  /* You can add additional styling here */
   </style>
   
 
