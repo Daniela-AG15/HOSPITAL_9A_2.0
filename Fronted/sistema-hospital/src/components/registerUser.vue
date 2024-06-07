0d4488b1c60db2e39ece8d3754d86066a6d98573
Daniela-AG15 <danaguilar1512@gmail.com>
2024-06-07 13:15:45 -0600

    up

--- Fronted/sistema-hospital/src/components/registerUser.vue
+++ Fronted/sistema-hospital/src/components/registerUser.vue
@@ -1,7 +1,6 @@
 <template>
 	<form>
 	  <div class="py-16">
-		<form>
 		<div class="flex bg-white rounded-lg shadow-lg overflow-hidden mx-auto max-w-sm lg:max-w-4xl">
 		  <!-- Col -->
 		  <div class="w-full lg:w-5/12 bg-cover rounded-l-lg" style="background-image: url(https://i.pinimg.com/550x/f9/13/05/f9130503f61225d775facb9047d7ca92.jpg)">
@@ -17,7 +16,6 @@
 			</div>
 		  </div>
 		</div>
-		</form>
 	  </div>
 	</form>
   </template>
@@ -49,5 +47,3 @@
   /* You can add additional styling here */
   </style>
   
-
-
