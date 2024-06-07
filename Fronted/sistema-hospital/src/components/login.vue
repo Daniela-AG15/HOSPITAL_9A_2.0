6d641a9ceea7563d11139846fc416d388f35b5f5
Daniela-AG15 <161864911+Daniela-AG15@users.noreply.github.com>
2024-06-07 12:29:45 -0600

    Add files via upload

--- Fronted/sistema-hospital/src/components/login.vue
+++ Fronted/sistema-hospital/src/components/login.vue
@@ -44,7 +44,7 @@
                 <input class="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none" type="password" />
             </div>
             <div class="mt-8">
-                <RouterLink to="/login">
+                <RouterLink to="/dashboard">
                 <button class="bg-gray-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-gray-600">Iniciar Sesion</button>
                 </RouterLink>
             </div>
